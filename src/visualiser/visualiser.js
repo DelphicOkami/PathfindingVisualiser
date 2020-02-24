3e833a7788281ed6af717afafc3e772865f9ec3a
Xaithe <tgulliver97@gmail.com>
2020-02-24 21:15:12 +0000

    added props to square, moved grid generation to "visualiser"

--- src/visualiser/visualiser.js
+++ src/visualiser/visualiser.js
@@ -2,58 +2,109 @@ import React, {Component} from 'react';
 
 import './visualiser.css';
 
+
 class Square extends React.Component {
     // TODO state?
     render() {
+        //console.log(this.props);
+        const classes = this.props.isFinish
+        ? 'end'
+        : this.props.isStart
+        ? 'start'
+        : this.props.isWall
+        ? 'wall'
+        : '';
     return (
-     <div className="square"></div>   
+     <div className={`square ${classes}`}></div>   
     )
     }
 }
 
 class Grid extends React.Component {
-    renderSquare(i) {
-        return (
-            <Square
-                value={i}
-            />
-        );
-    }
-
-    render() {
-        // TODO grab these values from DOM to handle responsiveness
-        const viewWidth = 1920.0;
-        const viewHeight = 952.0;
-        const gridWidth = viewWidth * 0.9;
-        const gridHeight = viewHeight * 0.9;
-
-        const numberrows = Math.floor(gridHeight / 27);
-        const rowlength = Math.ceil(gridWidth / 27) + 1;
-
-        let rows = [];
-        let count = 0;
-
-        for (let i = 0; i < numberrows; i++){
-            let filledRows = [];
-            for (let j = 0; j < rowlength; j++) {
-                filledRows.push(this.renderSquare(count));
-                count ++;
-            }
-            rows.push(<div className="row" key={i}>{filledRows}</div>);
     
+    render() {
+        const rows = this.props.rows
+        const renderedRows = [];
+        for (let i = 0; i < rows.length; i++){
+            renderedRows.push(<div className="row" key={i}>{rows[i]}</div>);
         }
-        return(rows);
+        return renderedRows;
     }
 }
 
 export default class Visualiser extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          viewWidth: 1920,
+          viewHeight: 952,
+          grid: [],
+          mousePressed: false,
+        };
+      }
+
+    componentDidMount() {
+        const grid = this.getBlankGrid(1920,952);
+        this.setState({grid});
+    }
+
     render() {
         return(
             <div className="visualiser">
                 <div className="grid">
-                    <Grid />
+                    <Grid 
+                    rows={this.state.grid}
+                    />
                 </div>
             </div>
         );
+    }
+
+    newSquare(row,col,start,finish) {
+        return (
+            <Square
+                row={row}
+                column={col}
+                isStart={start}
+                isFinish={finish}
+                isWall={false}
+                distance={null}
+            />
+        );
+    }
+
+    getBlankGrid(viewWidth, viewHeight) {
+        const gridWidth = viewWidth * 0.9;
+        const gridHeight = viewHeight * 0.9;
+        const rowLength = Math.ceil(gridWidth / 27) + 1;
+        const numberOfRows = Math.floor(gridHeight / 27);
+
+        const startRow = Math.round(numberOfRows / 2);
+        const startCol = Math.round(rowLength / 4);
+        const endCol = Math.round(rowLength / 4 * 3);
+
+        console.log("start: " + startRow + "," + startCol);
+        console.log("end: " + startRow + "," + endCol);
+
+        let rows = [];
+        let count = 0;
+
+        for (let i = 0; i < numberOfRows; i++) {
+            let filledRows = [];
+            for (let j = 0; j < rowLength; j++) {
+                if (i === startRow && j === startCol) {
+                    filledRows.push(this.newSquare(i,j,true,false));
+                } else if (i === startRow && j === endCol) {
+                    filledRows.push(this.newSquare(i,j,false,true));
+                } else {
+                    filledRows.push(this.newSquare(i,j,false,false));
+                }
+
+                count++;
+            }
+            rows.push(filledRows);
+        }
+        return rows;
+
     }
 }
